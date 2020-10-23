01b31e03cfa8d72ab59a238cf9ffb1c5bbb5e53c
mishikag <23mgupta@athenian.org>
2020-10-23 04:11:58 -0700

    progress on tictactoe

--- my_code.py
+++ my_code.py
@@ -2,55 +2,66 @@
 #  
 # A note on style: Dictionaries can be defined before or after functions.
 
-board = {'Tl':' ','TM':' ','TR':' ','ML':' ','MM':' ','MR':' ','BL':' ','BM':' ','BR':' '}
+the_board = {'TL': ' ','TM': ' ','TR': ' ','ML': ' ','MM': ' ','MR': ' ','BL': ' ','BM': ' ','BR': ' '}
 
-def intro_board(board):
-    print ('|    ' '|    ' '|    ')
+def display_board(theBoard):
+    print(theBoard['TL'], '  |',theBoard['TM'], ' |', '  ',theBoard['TR'])
     print ('-+-+-'+'-+-+-'+'-+-+-')
-    print ('|    ' '|    ' '|    ')
+    print(theBoard['ML'], '  |',theBoard['MM'], ' |', '  ',theBoard['MR'])
     print ('-+-+-'+'-+-+-'+'-+-+-')
-    print ('|    ' '|    ' '|    ')
+    print(theBoard['BL'], '  |',theBoard['BM'], ' |', '  ',theBoard['BR'])
     print ('-+-+-'+'-+-+-'+'-+-+-')
 
-def CheckWinner(board):
+#def intro_board(theBoard):
+    #print ('|    ' '|    ' '|    ')
+    #print ('-+-+-'+'-+-+-'+'-+-+-')
+    #print ('|    ' '|    ' '|    ')
+    #print ('-+-+-'+'-+-+-'+'-+-+-')
+    #print ('|    ' '|    ' '|    ')
+    #print ('-+-+-'+'-+-+-'+'-+-+-')
+
+def CheckWinner(the_board):
+    global check
     check = 0
     #use different board
-    if board['Tl'] == board['TM'] == board['TR'] != ' ' :
+    if the_board['Tl'] == the_board['TM'] == the_board['TR'] != ' ' :
         check = 1
-    elif board['TL'] == board['ML'] == board['BL'] != ' ':
+    elif the_board['TL'] == the_board['ML'] == the_board['BL'] != ' ':
         check = 1
-    elif board['TL'] == board['MM'] == board['BR'] != ' ':
+    elif the_board['TL'] == the_board['MM'] == the_board['BR'] != ' ':
         check = 1
-    elif board['ML'] == board['MM'] == board['MR'] != ' ':
+    elif the_board['ML'] == the_board['MM'] == the_board['MR'] != ' ':
         check = 1
-    elif board['BL'] == board['BM'] == board['BR'] != ' ':
+    elif the_board['BL'] == the_board['BM'] == the_board['BR'] != ' ':
         check = 1
-    elif board['BL'] == board['MM'] == board['TR'] != ' ':
+    elif the_board['BL'] == the_board['MM'] == the_board['TR'] != ' ':
         check = 1
-    elif board['TR'] == board['MR'] == board['BR'] != ' ':
+    elif the_board['TR'] == the_board['MR'] == the_board['BR'] != ' ':
         check = 1
-    elif board['BM'] == board['MM'] == board['TM'] != ' ':
+    elif the_board['BM'] == the_board['MM'] == the_board['TM'] != ' ':
         check = 1
     return check
 
-def play(board):
+def game(the_board):
     a = "X"
     for i in range(9):
-        # different function board to display spots taken
+        display_board(the_board)
         print("it is", a, "turn")
         turns = input("enter a place in the board")
         while True:
-            if board[turns]==' ':
-                board[turns] == a
+            if the_board[turns]==' ':
+                the_board[turns] == a
                 break
             else:
                 print("sorry this spot is already taken. place your", a, "somewehere else")
-                # different function board to display spots taken
+                display_board(the_board)
                 turns = input("enter a place in the board")
-
-
-
-print(intro_board(board))
-
-
-
+            check == CheckWinner(the_board)
+            if check == 1:
+                print("the winner is", a)
+                break
+            else:
+                print("there is no winner")
+                break
+game(the_board)
+display_board(the_board)
